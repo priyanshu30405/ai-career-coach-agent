8a9a36957ba45552eb353d2c5306f860e7fd2483
priyanshu30405 <priyanshuraj30405@gmail.com>
2025-07-07 23:41:06 +0530

    first commit

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,9 +1,7 @@
 "use client"
-import { AuthContext } from '@/context/AuthContext';
 import { useUser } from '@clerk/nextjs';
 import axios from 'axios';
-import React, { useContext, useEffect, useState } from 'react'
-
+import React, { useEffect } from 'react';
 
 function Provider({
     children,
@@ -18,7 +16,6 @@ function Provider({
 
     const createNewUser = async () => {
         const result = await axios.post('/api/user');
-
     }
 
     return (
@@ -27,13 +24,6 @@ function Provider({
         </div>
     )
 }
-
-// Custom hook to use auth
-export const useAuthContext = () => {
-    const context = useContext(AuthContext);
-    if (!context) throw new Error("useAuth must be used within an AuthProvider");
-    return context;
-};
 
 export default Provider
 
